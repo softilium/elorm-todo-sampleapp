90a9c8bcabd70a7240f98ae7bfc580b39fd9ae1e
Nick Mokhnatov <softilium@gmail.com>
2025-07-14 13:51:48 +0200

    new way of handlers definitions

--- dbcontext_handlers.go
+++ dbcontext_handlers.go
@@ -3,42 +3,37 @@ package main
 import "github.com/softilium/elorm"
 
 func (dbc *DbContext) setHandlers() {
-	dbc.TodoItemDef.BeforeDeleteHandlerByRef = todo_BeforeDeleteByRef
-	dbc.UserDef.BeforeDeleteHandlerByRef = user_BeforeDeleteByRef
-}
-
-func todo_BeforeDeleteByRef(ref string) error {
-
-	comments, _, err := dbc.TodoCommentDef.SelectEntities(
-		[]*elorm.Filter{elorm.AddFilterEQ(dbc.TodoCommentDef.TodoItem, ref)},
-		nil, 0, 0)
-	if err != nil {
-		return err
-	}
-	for _, comment := range comments {
-		err = dbc.Factory.DeleteEntity(comment.RefString())
+	err := dbc.AddBeforeDeleteHandlerByRef(dbc.TodoItemDef.EntityDef, func(ref string) error {
+		comments, _, err := dbc.TodoCommentDef.SelectEntities(
+			[]*elorm.Filter{elorm.AddFilterEQ(dbc.TodoCommentDef.TodoItem, ref)},
+			nil, 0, 0)
 		if err != nil {
 			return err
 		}
-	}
+		for _, comment := range comments {
+			err = dbc.Factory.DeleteEntity(comment.RefString())
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+	logErr(err)
 
-	return nil
-}
-
-func user_BeforeDeleteByRef(ref string) error {
-
-	todos, _, err := dbc.TodoItemDef.SelectEntities(
-		[]*elorm.Filter{elorm.AddFilterEQ(dbc.TodoItemDef.Owner, ref)},
-		nil, 0, 0)
-	if err != nil {
-		return err
-	}
-	for _, t := range todos {
-		err = dbc.Factory.DeleteEntity(t.RefString())
+	err = dbc.AddBeforeDeleteHandlerByRef(dbc.UserDef.EntityDef, func(ref string) error {
+		todos, _, err := dbc.TodoItemDef.SelectEntities(
+			[]*elorm.Filter{elorm.AddFilterEQ(dbc.TodoItemDef.Owner, ref)},
+			nil, 0, 0)
 		if err != nil {
 			return err
 		}
-	}
-
-	return nil
+		for _, t := range todos {
+			err = dbc.Factory.DeleteEntity(t.RefString())
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+	logErr(err)
 }
